petrouz/muros

commit 8d55a5cb61eca22bfcd3fa06796f75b70c1e0276

IDS: "none" doesn't equal "", looks like a typo resulting in ignoring the original eve.json

src/opnsense/mvc/app/views/OPNsense/IDS/index.volt
@@ -141,7 +141,7 @@ POSSIBILITY OF SUCH DAMAGE.
             });
 
 
-            if ( selected_logfile != "") {
+            if ( selected_logfile != "none") {
                 request['fileid'] = selected_logfile;
                 request['rowCount'] = selected_max_entries;
                 request['searchPhrase'] = search_phrase;

commit 5977c9833d90828eaa3ec018edb78d13eb952b13

Services / Intrusion detection / Alerts - regression in https://github.com/opnsense/core/commit/8d55a5cb61eca22bfcd3fa06796f75b70c1e0276, ignoring filters and number of items to show

src/opnsense/mvc/app/views/OPNsense/IDS/index.volt
@@ -143,9 +143,9 @@ POSSIBILITY OF SUCH DAMAGE.
 
             if ( selected_logfile != "none") {
                 request['fileid'] = selected_logfile;
-                request['rowCount'] = selected_max_entries;
-                request['searchPhrase'] = search_phrase;
             }
+            request['rowCount'] = selected_max_entries;
+            request['searchPhrase'] = search_phrase;
             return request;
         }
 