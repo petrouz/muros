petrouz/muros

commit dd7cce234b54a9121c5962534b6aaa3152950dd8

Firewall / Aliases - add "virtual" properties to model representing the current pf table stats and represent these in the alias grid.

src/opnsense/mvc/app/models/OPNsense/Firewall/FieldTypes/AliasField.php
@@ -31,6 +31,10 @@
 namespace OPNsense\Firewall\FieldTypes;
 
 use OPNsense\Base\FieldTypes\ArrayField;
+use OPNsense\Base\FieldTypes\TextField;
+use OPNsense\Base\FieldTypes\IntegerField;
+use OPNsense\Core\Backend;
+
 
 class AliasField extends ArrayField
 {
@@ -64,6 +68,33 @@ class AliasField extends ArrayField
             "content" => ""
         ]
     ];
+
+    private static $current_stats = null;
+
+    protected function actionPostLoadingEvent()
+    {
+        if (self::$current_stats === null) {
+            self::$current_stats = [];
+            $stats = json_decode((new Backend())->configdRun('filter diag table_size'), true);
+            if (!empty($stats) && !empty($stats['details'])) {
+                self::$current_stats = $stats['details'];
+            }
+        }
+        foreach ($this->internalChildnodes as $node) {
+            if (!$node->getInternalIsVirtual()) {
+                // generate new unattached fields, which are only usable to read data from (not synched to config.xml)
+                $current_items = new IntegerField();
+                $last_updated = new TextField();
+                if (!empty((string)$node->name) && !empty(self::$current_stats[(string)$node->name])) {
+                    $current_items->setValue(self::$current_stats[(string)$node->name]['count']);
+                    $last_updated->setValue(self::$current_stats[(string)$node->name]['updated']);
+                }
+                $node->addChildNode('current_items', $current_items);
+                $node->addChildNode('last_updated', $last_updated);
+            }
+        }
+        return parent::actionPostLoadingEvent();
+    }
 
     /**
      * create virtual alias nodes

src/opnsense/mvc/app/controllers/OPNsense/Firewall/Api/AliasController.php
@@ -60,7 +60,7 @@ class AliasController extends ApiMutableModelControllerBase
         }
         return $this->searchBase(
             "aliases.alias",
-            array('enabled', 'name', 'description', 'type', 'content'),
+            array('enabled', 'name', 'description', 'type', 'content', 'current_items', 'last_updated'),
             "name",
             $filter_funct
         );

src/opnsense/mvc/app/views/OPNsense/Firewall/alias.volt
@@ -513,6 +513,8 @@
                             <th data-column-id="type" data-width="12em" data-type="string">{{ lang._('Type') }}</th>
                             <th data-column-id="description" data-type="string">{{ lang._('Description') }}</th>
                             <th data-column-id="content" data-type="string">{{ lang._('Content') }}</th>
+                            <th data-column-id="current_items" data-type="string">{{ lang._('Loaded#') }}</th>
+                            <th data-column-id="last_updated" data-type="string">{{ lang._('Last updated') }}</th>
                             <th data-column-id="commands" data-width="7em" data-formatter="commands" data-sortable="false">{{ lang._('Commands') }}</th>
                         </tr>
                         </thead>

commit 533c6da63ed57c3d97b884563d294cf8024b7ada

regression in https://github.com/opnsense/core/commit/dd7cce234b54a9121c5962534b6aaa3152950dd8 (Firewall / Aliases - add "virtual" properties to model representing the current pf table stats and represent these in the alias grid.)
- forgot to mark fields virtual, which triggers a model save to create xml attributes
- if fields are missing in the datastream, UIModelGrid would crash out.

src/opnsense/mvc/app/library/OPNsense/Base/UIModelGrid.php
@@ -150,7 +150,10 @@ class UIModelGrid
                 if ($searchPhrase != '') {
                     $searchFound = false;
                     foreach ($fields as $fieldname) {
-                        if (strpos(strtolower($row[$fieldname]), strtolower($searchPhrase)) !== false) {
+                        if (
+                            isset($row[$fieldname]) &&
+                            strpos(strtolower($row[$fieldname]), strtolower($searchPhrase)) !== false
+                        ) {
                             $searchFound = true;
                             break;
                         }

src/opnsense/mvc/app/models/OPNsense/Firewall/FieldTypes/AliasField.php
@@ -83,7 +83,9 @@ class AliasField extends ArrayField
             if (!$node->getInternalIsVirtual()) {
                 // generate new unattached fields, which are only usable to read data from (not synched to config.xml)
                 $current_items = new IntegerField();
+                $current_items->setInternalIsVirtual();
                 $last_updated = new TextField();
+                $last_updated->setInternalIsVirtual();
                 if (!empty((string)$node->name) && !empty(self::$current_stats[(string)$node->name])) {
                     $current_items->setValue(self::$current_stats[(string)$node->name]['count']);
                     $last_updated->setValue(self::$current_stats[(string)$node->name]['updated']);