opnsense/core

commit ad9b7b1f400b83267cdc76491332596e6c1e8f36

Firewall / Aliases - various usability and visibility improvements (#5668)
Firewall / Aliases - various usability and visibility improvements

o change /api/firewall/alias/listNetworkAliases endpoint to return name and address
o add alias description as subtext in network group dropdown
o exclude row buttons for internal aliases
o support nesting of external aliases
o attach statistics to external aliases (like bogons and new interface network types)
o add preprocess in alias to handle non gui defined types
o network aliases will flush :network into the table
o aliases which aren't managed via configured settings will be fetched for nesting
o gather pf tables which aren't generated into filter_tables.conf as being external so the new imported static_aliases are usable without the need to import the settings in the template language
o initial work to support interface networks, register internal types and flush to alias template
o support imported static aliases using json definitions and move core aliases in there

src/opnsense/mvc/app/models/OPNsense/Firewall/FieldTypes/AliasField.php
@@ -34,41 +34,52 @@ use OPNsense\Base\FieldTypes\ArrayField;
 use OPNsense\Base\FieldTypes\TextField;
 use OPNsense\Base\FieldTypes\IntegerField;
 use OPNsense\Core\Backend;
+use OPNsense\Core\Config;
 
 class AliasField extends ArrayField
 {
-    private static $reservedItems = [
-        "bogons" => [
-            "enabled" => "1",
-            "name" => "bogons",
-            "type" => "external",
-            "description" => "bogon networks (internal)",
-            "content" => ""
-        ],
-        "bogonsv6" => [
-            "enabled" => "1",
-            "name" => "bogonsv6",
-            "type" => "external",
-            "description" => "bogon networks IPv6 (internal)",
-            "content" => ""
-        ],
-        "virusprot" => [
-            "enabled" => "1",
-            "name" => "virusprot",
-            "type" => "external",
-            "description" => "overload table for rate limiting (internal)",
-            "content" => ""
-        ],
-        "sshlockout" => [
-            "enabled" => "1",
-            "name" => "sshlockout",
-            "type" => "external",
-            "description" => "abuse lockout table (internal)",
-            "content" => ""
-        ]
-    ];
+    private static $reservedItems = [];
 
     private static $current_stats = null;
+
+    public function __construct($ref = null, $tagname = null)
+    {
+        foreach (glob(__DIR__ . "/../static_aliases/*.json") as $filename) {
+            $payload = json_decode(file_get_contents($filename), true);
+            if (is_array($payload)) {
+                foreach ($payload as $aliasname => $content) {
+                    self::$reservedItems[$aliasname] = $content;
+                }
+            }
+        }
+        foreach (Config::getInstance()->object()->interfaces->children() as $k => $n) {
+            $table_name = sprintf("__%s_network", $k);
+            $table_desc = !empty((string)$n->descr) ? (string)$n->descr : $k;
+            self::$reservedItems[$table_name] = [
+                "enabled" => "1",
+                "name" => $table_name,
+                "type" => "internal",
+                "description" => sprintf("%s %s", $table_desc, gettext("net")),
+                "content" => ""
+            ];
+        }
+        parent::__construct($ref, $tagname);
+    }
+
+    private function addStatsFields($node)
+    {
+        // generate new unattached fields, which are only usable to read data from (not synched to config.xml)
+        $current_items = new IntegerField();
+        $current_items->setInternalIsVirtual();
+        $last_updated = new TextField();
+        $last_updated->setInternalIsVirtual();
+        if (!empty((string)$node->name) && !empty(self::$current_stats[(string)$node->name])) {
+            $current_items->setValue(self::$current_stats[(string)$node->name]['count']);
+            $last_updated->setValue(self::$current_stats[(string)$node->name]['updated']);
+        }
+        $node->addChildNode('current_items', $current_items);
+        $node->addChildNode('last_updated', $last_updated);
+    }
 
     protected function actionPostLoadingEvent()
     {
@@ -81,17 +92,7 @@ class AliasField extends ArrayField
         }
         foreach ($this->internalChildnodes as $node) {
             if (!$node->getInternalIsVirtual()) {
-                // generate new unattached fields, which are only usable to read data from (not synched to config.xml)
-                $current_items = new IntegerField();
-                $current_items->setInternalIsVirtual();
-                $last_updated = new TextField();
-                $last_updated->setInternalIsVirtual();
-                if (!empty((string)$node->name) && !empty(self::$current_stats[(string)$node->name])) {
-                    $current_items->setValue(self::$current_stats[(string)$node->name]['count']);
-                    $last_updated->setValue(self::$current_stats[(string)$node->name]['updated']);
-                }
-                $node->addChildNode('current_items', $current_items);
-                $node->addChildNode('last_updated', $last_updated);
+                $this->addStatsFields($node);
             }
         }
         return parent::actionPostLoadingEvent();
@@ -115,6 +116,7 @@ class AliasField extends ArrayField
                 }
                 $container_node->addChildNode($key, $node);
             }
+            $this->addStatsFields($container_node);
             $result[$aliasName] = $container_node;
         }
         return $result;

src/etc/inc/legacy_bindings.inc
@@ -88,13 +88,16 @@ function legacy_html_escape_form_data(&$settings)
  */
 function legacy_list_aliases($type)
 {
-    $result = array();
+    $result = [];
 
     foreach ((new \OPNsense\Firewall\Alias())->aliasIterator() as $alias) {
-        if ($type == "port" && preg_match("/port/i", $alias['type'])) {
-            $result[] = array('name' => $alias['name'], 'type' => $alias['type']);
+        if ($alias['type'] == 'internal') {
+            // XXX: skip internal aliases while still defined as "networks" in the rules pages
+            continue;
+        } elseif ($type == "port" && preg_match("/port/i", $alias['type'])) {
+            $result[] = ['name' => $alias['name'], 'type' => $alias['type']];
         } elseif ($type != "port" && !preg_match("/port/i", $alias['type'])) {
-            $result[] = array('name' => $alias['name'], 'type' => $alias['type']);
+            $result[] = ['name' => $alias['name'], 'type' => $alias['type']];
         }
     }
 

src/opnsense/mvc/app/controllers/OPNsense/Firewall/Api/AliasController.php
@@ -109,12 +109,13 @@ class AliasController extends ApiMutableModelControllerBase
         $response = $this->getBase("alias", "aliases.alias", $uuid);
         $selected_aliases = array_keys($response['alias']['content']);
         foreach ($this->getModel()->aliasIterator() as $alias) {
-            // external aliases can't be nested (always empty according to our administration)
-            if (!in_array($alias['name'], $selected_aliases) && $alias['type'] != "external") {
+            if (!in_array($alias['name'], $selected_aliases)) {
                 $response['alias']['content'][$alias['name']] = [
                   "selected" => 0, "value" => $alias['name']
                 ];
             }
+            // append descriptions
+            $response['alias']['content'][$alias['name']]['description'] = $alias['description'];
         }
         return $response;
     }
@@ -216,10 +217,13 @@ class AliasController extends ApiMutableModelControllerBase
      */
     public function listNetworkAliasesAction()
     {
-        $result = array();
+        $result = [];
         foreach ($this->getModel()->aliases->alias->iterateItems() as $alias) {
             if (!in_array((string)$alias->type, ['external', 'port'])) {
-                $result[(string)$alias->name] = (string)$alias->name;
+                $result[(string)$alias->name] = [
+                    "name" => (string)$alias->name,
+                    "description" => (string)$alias->description
+                ];
             }
         }
         ksort($result);

src/opnsense/mvc/app/models/OPNsense/Firewall/Alias.php
@@ -162,25 +162,12 @@ class Alias extends BaseModel
      */
     public function aliasIterator()
     {
-        $use_legacy = true;
         foreach ($this->aliases->alias->iterateItems() as $alias) {
             $record = array();
             foreach ($alias->iterateItems() as $key => $value) {
                 $record[$key] = (string)$value;
             }
             yield $record;
-            $use_legacy = false;
-        }
-        // MVC not used (yet) return legacy type aliases
-        if ($use_legacy) {
-            $cfgObj = Config::getInstance()->object();
-            if (!empty($cfgObj->aliases->alias)) {
-                foreach ($cfgObj->aliases->children() as $alias) {
-                    $alias = (array)$alias;
-                    $alias['content'] = !empty($alias['address']) ? str_replace(" ", "\n", $alias['address']) : null;
-                    yield $alias;
-                }
-            }
         }
     }
 

src/opnsense/mvc/app/models/OPNsense/Firewall/Alias.xml
@@ -37,6 +37,7 @@
                         <networkgroup>Network group</networkgroup>
                         <mac>MAC address</mac>
                         <external>External (advanced)</external>
+                        <internal>Internal (product)</internal>
                         <dynipv6host>Dynamic IPv6 Host</dynipv6host>
                     </OptionValues>
                     <Constraints>

src/opnsense/mvc/app/views/OPNsense/Firewall/alias.volt
@@ -43,6 +43,16 @@
                         request['type'] = $('#type_filter').val();
                     }
                     return request;
+                },
+                formatters: {
+                    "commands": function (column, row) {
+                        if (row.uuid.includes('-') === true) {
+                            // exclude buttons for internal aliases (which uses names instead of valid uuid's)
+                            return '<button type="button" class="btn btn-xs btn-default command-edit bootgrid-tooltip" data-row-id="' + row.uuid + '"><span class="fa fa-fw fa-pencil"></span></button> ' +
+                                '<button type="button" class="btn btn-xs btn-default command-copy bootgrid-tooltip" data-row-id="' + row.uuid + '"><span class="fa fa-fw fa-clone"></span></button>' +
+                                '<button type="button" class="btn btn-xs btn-default command-delete bootgrid-tooltip" data-row-id="' + row.uuid + '"><span class="fa fa-fw fa-trash-o"></span></button>';
+                        }
+                    },
                 }
             }
         });
@@ -57,7 +67,9 @@
             ajaxGet("/api/firewall/alias/listNetworkAliases", {}, function(data){
                 $("#network_content").empty();
                 $.each(data, function(alias, value) {
-                    $("#network_content").append($("<option/>").val(alias).text(value));
+                    let $opt = $("<option/>").val(alias).text(value.name);
+                    $opt.data('subtext', value.description);
+                    $("#network_content").append($opt);
                 });
                 $("#network_content").selectpicker('refresh');
             });
@@ -508,6 +520,7 @@
                                 <option value="networkgroup">{{ lang._('Network group') }}</option>
                                 <option value="dynipv6host">{{ lang._('Dynamic IPv6 Host') }}</option>
                                 <option value="external">{{ lang._('External (advanced)') }}</option>
+                                <option value="internal">{{ lang._('Internal (product)') }}</option>
                             </select>
                         </div>
                     </div>

src/opnsense/scripts/filter/lib/alias.py
@@ -1,5 +1,5 @@
 """
-    Copyright (c) 2017-2021 Ad Schellevis <ad@opnsense.org>
+    Copyright (c) 2017-2022 Ad Schellevis <ad@opnsense.org>
     All rights reserved.
 
     Redistribution and use in source and binary forms, with or without
@@ -33,6 +33,7 @@ import requests
 import ipaddress
 import dns.resolver
 import syslog
+import subprocess
 from hashlib import md5
 from dns.exception import DNSException
 from . import geoip
@@ -240,9 +241,10 @@ class Alias(object):
                 else:
                     undo_content = ""
                 try:
+                    self._resolve_content = self.pre_process()
                     address_parser = self.get_parser()
-                    for item in self.items():
-                        if address_parser:
+                    if address_parser:
+                        for item in self.items():
                             for address in address_parser(item):
                                 self._resolve_content.add(address)
                     # resolve hostnames (async) if there are any in the collected set
@@ -263,7 +265,7 @@ class Alias(object):
         return list(self._resolve_content)
 
     def get_parser(self):
-        """ fetch address parser to use, None if alias type is not handled here
+        """ fetch address parser to use, None if alias type is not handled here or only during pre processing
             :return: function or None
         """
         if self._type in ['host', 'network', 'networkgroup']:
@@ -278,6 +280,24 @@ class Alias(object):
             return ArpCache().iter_addresses
         else:
             return None
+
+    def pre_process(self):
+        """ alias type pre processors
+            :return: set initial alias content
+        """
+        result = set()
+        if self.get_type() == 'interface_net':
+            subprocess.run(
+                ['/sbin/pfctl', '-t', self.get_name(), '-T', 'replace', '%s:network' % self._interface],
+                capture_output=True
+            )
+        # collect current table contents for selected types
+        if self.get_type() in ['interface_net', 'external']:
+            pfctl_cmd = ['/sbin/pfctl', '-t', self.get_name(), '-T', 'show']
+            for line in subprocess.run(pfctl_cmd, capture_output=True, text=True).stdout.split('\n'):
+                result.add(line.strip())
+
+        return result
 
     def get_type(self):
         """ get type of alias

src/opnsense/scripts/filter/update_tables.py
@@ -1,7 +1,7 @@
 #!/usr/local/bin/python3
 
 """
-    Copyright (c) 2017-2019 Ad Schellevis <ad@opnsense.org>
+    Copyright (c) 2017-2022 Ad Schellevis <ad@opnsense.org>
     All rights reserved.
 
     Redistribution and use in source and binary forms, with or without
@@ -55,8 +55,14 @@ class AliasParser(object):
             :return: None
         """
         self._aliases = dict()
+        external_aliases = list()
         alias_parameters = dict()
         alias_parameters['known_aliases'] = [x.text for x in self._source_tree.iterfind('table/name')]
+        for line in  subprocess.run(['/sbin/pfctl', '-sT'], capture_output=True, text=True).stdout.strip().split('\n'):
+            alias_name = line.strip()
+            if alias_name not in alias_parameters['known_aliases']:
+                alias_parameters['known_aliases'].append(alias_name)
+                external_aliases.append(alias_name)
 
         # parse general alias settings
         conf_general = self._source_tree.find('general')
@@ -64,10 +70,18 @@ class AliasParser(object):
             if conf_general.find('ssl_no_verify') is not None and conf_general.find('ssl_no_verify').text == "1":
                 alias_parameters['ssl_no_verify'] = True
 
-        # loop through aliases
+        # loop through user defined aliases
         for elem in self._source_tree.iterfind('table'):
             alias = Alias(elem, **alias_parameters)
             self._aliases[alias.get_name()] = alias
+
+        # attach external aliases which aren't defined via the gui
+        for alias_name in external_aliases:
+            elem = ET.Element("table")
+            ET.SubElement(elem, 'type').text = 'external'
+            ET.SubElement(elem, 'name').text = alias_name
+            ET.SubElement(elem, 'ttl').text = '1'
+            self._aliases[alias_name] = Alias(elem, **alias_parameters)
 
     def get_alias_deps(self, alias, alias_deps=None):
         """ recursive fetch all alias dependencies
@@ -141,36 +155,36 @@ if __name__ == '__main__':
             open('/var/db/aliastables/%s.txt' % alias_name, 'w').write('\n'.join(sorted(alias_content)))
 
         alias_pf_content = list()
-        sp = subprocess.run(['/sbin/pfctl', '-t', alias_name, '-T', 'show'], capture_output=True, text=True)
-        for line in sp.stdout.strip().split('\n'):
-            line = line.strip()
-            if line:
-                alias_pf_content.append(line)
-        if (len(alias_content) != len(alias_pf_content) or alias_changed_or_expired) and alias.get_parser():
-            # if the alias is changed, expired or the one in memory has a different number of items, load table
-            # (but only if we know how to handle this alias type)
-            if len(alias_content) == 0:
-                # flush when target is empty
-                subprocess.run(['/sbin/pfctl', '-t', alias_name, '-T', 'flush'], capture_output=True)
-            else:
-                # replace table contents with collected alias
-                sp = subprocess.run(['/sbin/pfctl', '-t', alias_name, '-T', 'replace', '-f',
-                                     '/var/db/aliastables/%s.txt' % alias_name], capture_output=True, text=True)
+        if alias.get_parser():
+            # only try to replace the contents of this alias if we're responsible for it (know how to parse)
+            sp = subprocess.run(['/sbin/pfctl', '-t', alias_name, '-T', 'show'], capture_output=True, text=True)
+            for line in sp.stdout.strip().split('\n'):
+                alias_pf_content.append(line.strip())
 
-                error_output = sp.stderr.strip()
-                if error_output.find('pfctl: ') > -1:
-                    error_message = "Error loading alias [%s]: %s {current_size: %d, new_size: %d}" % (
-                        alias_name,
-                        error_output.replace('pfctl: ', ''),
-                        len(alias_pf_content),
-                        len(alias_content),
-                    )
-                    result['status'] = 'error'
-                    if 'messages' not in result:
-                        result['messages'] = list()
-                    if error_output not in result['messages']:
-                        result['messages'].append(error_message)
-                        syslog.syslog(syslog.LOG_NOTICE, error_message)
+            if (len(alias_content) != len(alias_pf_content) or alias_changed_or_expired):
+                # if the alias is changed, expired or the one in memory has a different number of items, load table
+                if len(alias_content) == 0:
+                    # flush when target is empty
+                    subprocess.run(['/sbin/pfctl', '-t', alias_name, '-T', 'flush'], capture_output=True)
+                else:
+                    # replace table contents with collected alias
+                    sp = subprocess.run(['/sbin/pfctl', '-t', alias_name, '-T', 'replace', '-f',
+                                         '/var/db/aliastables/%s.txt' % alias_name], capture_output=True, text=True)
+
+                    error_output = sp.stderr.strip()
+                    if error_output.find('pfctl: ') > -1:
+                        error_message = "Error loading alias [%s]: %s {current_size: %d, new_size: %d}" % (
+                            alias_name,
+                            error_output.replace('pfctl: ', ''),
+                            len(alias_pf_content),
+                            len(alias_content),
+                        )
+                        result['status'] = 'error'
+                        if 'messages' not in result:
+                            result['messages'] = list()
+                        if error_output not in result['messages']:
+                            result['messages'].append(error_message)
+                            syslog.syslog(syslog.LOG_NOTICE, error_message)
 
     # cleanup removed aliases
     to_remove = dict()

petrouz/muros

commit a955e698d4f644aaed04ccfdb5cccf19671cf4b7

Firewall / Aliases - add markUnchanged() in BaseField type so we can avoid validation on virtual fields  (issue introduced in https://github.com/opnsense/core/pull/5668)

src/opnsense/mvc/app/models/OPNsense/Base/FieldTypes/BaseField.php
@@ -372,6 +372,14 @@ abstract class BaseField
     }
 
     /**
+     * force field to act as unchanged (skip validations)
+     */
+    public function markUnchanged()
+    {
+        $this->internalInitialValue = $this->internalValue;
+    }
+
+    /**
      * check if field content has changed
      * @return bool change indicator
      */

src/opnsense/mvc/app/models/OPNsense/Firewall/FieldTypes/AliasField.php
@@ -114,6 +114,7 @@ class AliasField extends ArrayField
                 if (isset($aliasContent[$key])) {
                     $node->setValue($aliasContent[$key]);
                 }
+                $node->markUnchanged();
                 $container_node->addChildNode($key, $node);
             }
             $this->addStatsFields($container_node);