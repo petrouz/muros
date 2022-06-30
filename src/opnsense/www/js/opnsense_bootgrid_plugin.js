petrouz/muros

commit 13b1290b50f4f127329b1d63ea0cb9cf56ae9cd2

MVC / bootgrid - merge option objects in stead of replacing them. closes https://github.com/opnsense/core/pull/5833

src/opnsense/www/js/opnsense_bootgrid_plugin.js
@@ -140,7 +140,11 @@ $.fn.UIBootgrid = function (params) {
         // merge additional options (if any)
         if (params['options'] !== undefined) {
             $.each(params['options'],  function(key, value) {
-                gridopt[key] = value;
+                if (typeof(value) === 'object') {
+                    gridopt[key] = Object.assign({}, gridopt[key], value);
+                } else {
+                    gridopt[key] = value;
+                }
             });
         }
 

commit 499b9c5499d9af859e012d530548f3ea588945e3

MVC - bootgrid - regression from 13b1290 (#5840)
* Exclude if array, primarily for rowCount

src/opnsense/www/js/opnsense_bootgrid_plugin.js
@@ -140,7 +140,7 @@ $.fn.UIBootgrid = function (params) {
         // merge additional options (if any)
         if (params['options'] !== undefined) {
             $.each(params['options'],  function(key, value) {
-                if (typeof(value) === 'object') {
+                if (typeof(value) === 'object' && Array.isArray(value) == false) {
                     gridopt[key] = Object.assign({}, gridopt[key], value);
                 } else {
                     gridopt[key] = value;