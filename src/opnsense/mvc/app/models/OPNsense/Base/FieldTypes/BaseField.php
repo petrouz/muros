petrouz/muros

commit 226a6a4d2233ca55b851269e6e2bb20dee1c3b53

MVC: fix two regressions and deprecate __items
Before the phalcon5 compatibility layer, the unit tests were broken due to __isset()
being triggered by an isset call on an __items attribute in getConstraintValidators().
Since __items is only used in BaseField itself, we choose to remove it for further use. Instead,
we rely on __isset() to verify if elements exist in internalChildNodes when a subnode is accessed directly.

The second regression lived in validate() in the custom Validation implementation. Instead
of relying on the incoming data as a key, we iterate through all validators active during the objects' lifetime since
these are set prior to validation. This regression affected all constraints.

src/opnsense/mvc/app/models/OPNsense/Base/FieldTypes/BaseField.php
@@ -42,7 +42,6 @@ use SimpleXMLElement;
  * @property-read string $__reference this tag absolute reference (node.subnode.subnode)
  * @property-read string $__type this tag's class Name ( example TextField )
  * @property-read string $__Ixx get tag by index/name even if the name is a number
- * @property-read array $__items this node's children
  */
 abstract class BaseField
 {
@@ -270,7 +269,7 @@ abstract class BaseField
 
     /**
      * Reflect default getter to internal child nodes.
-     * Implements the special attribute __items to return all items and __reference to identify the field in this model.
+     * Implements __reference to identify the field in this model.
      * @param string $name property name
      * @return mixed
      */
@@ -278,13 +277,6 @@ abstract class BaseField
     {
         if (isset($this->internalChildnodes[$name])) {
             return $this->internalChildnodes[$name];
-        } elseif ($name == '__items') {
-            // return all (no virtual/hidden) items
-            $result = array();
-            foreach ($this->iterateItems() as $key => $value) {
-                $result[$key] = $value;
-            }
-            return $result;
         } elseif ($name == '__reference') {
             return $this->internalReference;
         } elseif ($name == '__type') {
@@ -506,7 +498,7 @@ abstract class BaseField
                 $parentNode = $this->getParentNode();
                 if (count($parts) == 2) {
                     $tagName = $parts[0];
-                    if (isset($parentNode->__items[$tagName])) {
+                    if (isset($parentNode->$tagName) && !$parentNode->$tagName->GetInternalIsVirtual()) {
                         $ref_constraint = $parentNode->$tagName->getConstraintByName($parts[1]);
                         if ($ref_constraint != null) {
                             $result[] = $ref_constraint;
@@ -637,7 +629,7 @@ abstract class BaseField
         // add new items to array type objects
         if ($this->isArrayType()) {
             foreach ($data as $dataKey => $dataValue) {
-                if (!isset($this->__items[$dataKey])) {
+                if (!isset($this->$dataKey) && !$this->$dataKey->GetInternalIsVirtual()) {
                     $node = $this->add();
                     $node->setNodes($dataValue);
                 }

src/opnsense/mvc/app/models/OPNsense/Base/Validation.php
@@ -63,7 +63,7 @@ class Validation
      */
     public function add($key, $validator)
     {
-        if (is_a($validator, "OPNsense\Base\BaseValidator")) {
+        if (is_a($validator, "OPNsense\\Base\\BaseValidator")) {
             if (empty($this->validators[$key])) {
                 $this->validators[$key] = [];
             }
@@ -81,14 +81,16 @@ class Validation
      */
     public function validate($data)
     {
+        $validatorData = $this->validators;
         $this->data = $data;
-        foreach ($data as $key => $value) {
-            if (!empty($this->validators[$key])) {
-                foreach ($this->validators[$key] as $validator) {
-                    $validator->validate($this, $key);
-                }
+
+        foreach ($validatorData as $field => $validators) {
+            foreach ($validators as $validator) {
+                $validator->validate($this, $field);
             }
+
         }
+
         // XXX: temporary dual validation
         $phalconMsgs = $this->phalcon_validation->validate($data);
         if (!empty($phalconMsgs)) {

commit 369f15fc2b2937b6cb7d2a787f483f548e6dd822

MVC / BaseField - two regressions in https://github.com/opnsense/core/commit/226a6a4d2233ca55b851269e6e2bb20dee1c3b53
o GetInternalIsVirtual --> getInternalIsVirtual
o spurious getInternalIsVirtual() in setNodes() on null object

for https://github.com/opnsense/core/issues/5874

src/opnsense/mvc/app/models/OPNsense/Base/FieldTypes/BaseField.php
@@ -498,7 +498,7 @@ abstract class BaseField
                 $parentNode = $this->getParentNode();
                 if (count($parts) == 2) {
                     $tagName = $parts[0];
-                    if (isset($parentNode->$tagName) && !$parentNode->$tagName->GetInternalIsVirtual()) {
+                    if (isset($parentNode->$tagName) && !$parentNode->$tagName->getInternalIsVirtual()) {
                         $ref_constraint = $parentNode->$tagName->getConstraintByName($parts[1]);
                         if ($ref_constraint != null) {
                             $result[] = $ref_constraint;
@@ -629,7 +629,7 @@ abstract class BaseField
         // add new items to array type objects
         if ($this->isArrayType()) {
             foreach ($data as $dataKey => $dataValue) {
-                if (!isset($this->$dataKey) && !$this->$dataKey->GetInternalIsVirtual()) {
+                if (!isset($this->$dataKey)) {
                     $node = $this->add();
                     $node->setNodes($dataValue);
                 }