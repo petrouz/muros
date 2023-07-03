petrouz/muros

commit 2ec6db9f81ed427a37af641648bf2d55b051d8ef

MVC/Model/ArrayField - add generic static record definition.
While converting legacy code we do seem to have multiple static entries in different models, in which case we tend to overwrite ArrayField and extend the output with the static entries that are required for the model. These can either be defined in the legacy code (SPDField) or defined as static options (like the bogons in AliasField).

This commit adds a method getStaticChildren() which the derived class may overwrite in order to spawn new items to the list which can not be persisted. This should help avoid duplication of similar code constructs in various migrated areas. These items are constructed during the "post loading" event.

src/opnsense/mvc/app/models/OPNsense/Base/FieldTypes/ArrayField.php
@@ -40,6 +40,19 @@ class ArrayField extends BaseField
     private $internalTemplateNode = null;
 
     /**
+     * @var list statically defined children, key value store for static defined model entries
+     */
+    private static $internalStaticChildren = [];
+
+    /**
+     * @return key value store of static model items, overwrite when needed
+     */
+    protected static function getStaticChildren()
+    {
+        return [];
+    }
+
+    /**
      * Copy first node pointer as template node to make sure we always have a template to create new nodes from.
      * If the first node is virtual (no source data), remove that from the list.
      */
@@ -55,6 +68,22 @@ class ArrayField extends BaseField
             if ($this->internalChildnodes[$firstKey]->getInternalIsVirtual()) {
                 unset($this->internalChildnodes[$firstKey]);
             }
+        }
+        // init static entries when returned by getStaticChildren()
+        foreach (static::getStaticChildren() as $skey => $payload) {
+            $container_node = $this->newContainerField($this->__reference . "." . $skey, $this->internalXMLTagName);
+            $container_node->setAttributeValue("uuid", $skey);
+            $container_node->setInternalIsVirtual();
+            foreach ($this->getTemplateNode()->iterateItems() as $key => $value) {
+                $node = clone $value;
+                $node->setInternalReference($container_node->__reference . "." . $key);
+                if (isset($payload[$key])) {
+                    $node->setValue($payload[$key]);
+                }
+                $node->markUnchanged();
+                $container_node->addChildNode($key, $node);
+            }
+            self::$internalStaticChildren[$skey] = $container_node;
         }
     }
 
@@ -181,4 +210,42 @@ class ArrayField extends BaseField
 
         return array_values($sortedData);
     }
+
+    /**
+     * @inheritdoc
+     */
+    public function hasChild($name)
+    {
+        if (isset(self::$internalStaticChildren[$name])) {
+            return true;
+        } else {
+            return parent::hasChild($name);
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getChild($name)
+    {
+        if (isset(self::$internalStaticChildren[$name])) {
+            return self::$internalStaticChildren[$name];
+        } else {
+            return parent::getChild($name);
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function iterateItems()
+    {
+        foreach (parent::iterateItems() as $key => $value) {
+            yield $key => $value;
+        }
+        foreach (self::$internalStaticChildren as $key => $node) {
+            yield $key => $node;
+        }
+    }
+
 }

commit f43b12c47bb11f2534aed6fd6d6028573164f399

MVC/Model/ArrayField - fix scoping issue introduced in https://github.com/opnsense/core/commit/2ec6db9f81ed427a37af641648bf2d55b051d8ef

src/opnsense/mvc/app/models/OPNsense/Base/FieldTypes/ArrayField.php
@@ -42,7 +42,7 @@ class ArrayField extends BaseField
     /**
      * @var list statically defined children, key value store for static defined model entries
      */
-    private static $internalStaticChildren = [];
+    protected static $internalStaticChildren = [];
 
     /**
      * @return key value store of static model items, overwrite when needed
@@ -83,7 +83,7 @@ class ArrayField extends BaseField
                 $node->markUnchanged();
                 $container_node->addChildNode($key, $node);
             }
-            self::$internalStaticChildren[$skey] = $container_node;
+            static::$internalStaticChildren[$skey] = $container_node;
         }
     }
 
@@ -216,7 +216,7 @@ class ArrayField extends BaseField
      */
     public function hasChild($name)
     {
-        if (isset(self::$internalStaticChildren[$name])) {
+        if (isset(static::$internalStaticChildren[$name])) {
             return true;
         } else {
             return parent::hasChild($name);
@@ -228,8 +228,8 @@ class ArrayField extends BaseField
      */
     public function getChild($name)
     {
-        if (isset(self::$internalStaticChildren[$name])) {
-            return self::$internalStaticChildren[$name];
+        if (isset(static::$internalStaticChildren[$name])) {
+            return static::$internalStaticChildren[$name];
         } else {
             return parent::getChild($name);
         }
@@ -243,7 +243,7 @@ class ArrayField extends BaseField
         foreach (parent::iterateItems() as $key => $value) {
             yield $key => $value;
         }
-        foreach (self::$internalStaticChildren as $key => $node) {
+        foreach (static::$internalStaticChildren as $key => $node) {
             yield $key => $node;
         }
     }