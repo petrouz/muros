petrouz/muros

commit a457375eeffd044dd5a1e4bab9e4a00b43244aa8

mvc: refactor grid search to fetch descriptive values from the model instead of trying to reconstruct them.
This makes it easier for special model field types to translate values into human readable formats, also when not being presented to the user like that in a selector.

src/opnsense/mvc/app/models/OPNsense/Base/FieldTypes/BaseListField.php
@@ -122,6 +122,20 @@ abstract class BaseListField extends BaseField
         return $result;
     }
 
+    /**
+     * {@inheritdoc}
+     */
+    public function getDescription()
+    {
+        $items = [];
+        foreach ($this->getNodeData() as $fieldValue) {
+            if ($fieldValue['selected'] == 1) {
+                $items[] = $fieldValue['value'];
+            }
+        }
+        return implode(',',  $items);
+    }
+
 
     /**
      * {@inheritdoc}

src/opnsense/mvc/app/library/OPNsense/Base/UIModelGrid.php
@@ -127,22 +127,7 @@ class UIModelGrid
                 $row['uuid'] = $record->getAttributes()['uuid'];
                 foreach ($fields as $fieldname) {
                     if ($record->$fieldname != null) {
-                        $row[$fieldname] = $record->$fieldname->getNodeData();
-                        if (is_array($row[$fieldname])) {
-                            $listItems = $row[$fieldname];
-                            $row[$fieldname] = '';
-                            foreach ($listItems as $fieldValue) {
-                                if ($fieldValue['selected'] == 1) {
-                                    if ($row[$fieldname] != "") {
-                                        $row[$fieldname] .= ',';
-                                    }
-                                    $row[$fieldname] .= $fieldValue['value'];
-                                }
-                            }
-                            if ($row[$fieldname] === null) {
-                                $row[$fieldname] = "";
-                            }
-                        }
+                        $row[$fieldname] = $record->$fieldname->getDescription();
                     }
                 }
 

src/opnsense/mvc/app/models/OPNsense/Base/FieldTypes/BaseField.php
@@ -652,6 +652,17 @@ abstract class BaseField
     }
 
     /**
+     * Return descriptive value of the item.
+     * For simple types this is usually the internal value, complex types may return what this value represents.
+     * (descriptions of selected items)
+     * @return null|string
+     */
+    public function getDescription()
+    {
+        return (string)$this;
+    }
+
+    /**
      * update model with data returning missing repeating tag types.
      * @param $data array structure containing new model content
      * @throws Exception

commit 010df48f5594bbbd848823b9f6f938638d154014

mvc: minor regression in https://github.com/opnsense/core/commit/a457375eeffd044dd5a1e4bab9e4a00b43244aa8, if getNodeData() does not return an array, just pass the data.

src/opnsense/mvc/app/models/OPNsense/Base/FieldTypes/BaseListField.php
@@ -127,13 +127,18 @@ abstract class BaseListField extends BaseField
      */
     public function getDescription()
     {
-        $items = [];
-        foreach ($this->getNodeData() as $fieldValue) {
-            if ($fieldValue['selected'] == 1) {
-                $items[] = $fieldValue['value'];
+        $data = $this->getNodeData();
+        if (is_array($data)) {
+            $items = [];
+            foreach ($data as $fieldValue) {
+                if ($fieldValue['selected'] == 1) {
+                    $items[] = $fieldValue['value'];
+                }
             }
+            return implode(',', $items);
+        } else {
+            return $data;
         }
-        return implode(',', $items);
     }
 
 