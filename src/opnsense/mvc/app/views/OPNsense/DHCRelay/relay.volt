petrouz/muros

commit e1f3d43edcfe65cb3a9cfc56420e204723240be7

dhcp/dhcrelay: Integrate layout_partials/base_bootgrid_table (#8254)

src/opnsense/mvc/app/views/OPNsense/DHCRelay/relay.volt
@@ -26,14 +26,14 @@
 
 <script>
 $( document ).ready(function() {
-    $("#grid-dest").UIBootgrid({
+    $("#{{formGridDest['table_id']}}").UIBootgrid({
         search:'/api/dhcrelay/settings/searchDest',
         get:'/api/dhcrelay/settings/getDest/',
         set:'/api/dhcrelay/settings/setDest/',
         add:'/api/dhcrelay/settings/addDest/',
         del:'/api/dhcrelay/settings/delDest/',
     });
-    $("#grid-relay").UIBootgrid({
+    $("#{{formGridRelay['table_id']}}").UIBootgrid({
         search:'/api/dhcrelay/settings/searchRelay',
         get:'/api/dhcrelay/settings/getRelay/',
         set:'/api/dhcrelay/settings/setRelay/',
@@ -41,78 +41,43 @@ $( document ).ready(function() {
         del:'/api/dhcrelay/settings/delRelay/',
         toggle:'/api/dhcrelay/settings/toggleRelay/'
     });
+    $("div.actionBar").each(function(){
+        if ($(this).closest(".bootgrid-header").attr("id").includes("Dest")) {
+            $(this).parent().prepend($('<td id="heading-wrapper" class="col-sm-2 theading-text">{{ lang._('Destinations') }}</div>'));
+        } else {
+            $(this).parent().prepend($('<td id="heading-wrapper" class="col-sm-2 theading-text">{{ lang._('Relays') }}</div>'));
+        }
+    });
     $("#reconfigureAct").SimpleActionButton();
 });
 </script>
 
 <div class="content-box __mb">
-    <table class="table table-striped page-header" style="margin-top: 0">
-        <tbody><tr><th>Destinations</th><tr></tbody>
-    </table>
-    <table id="grid-dest" class="table table-condensed table-hover table-striped" data-editDialog="DialogDest">
-        <thead>
-        <tr>
-            <th data-column-id="uuid" data-type="string" data-identifier="true" data-visible="false">{{ lang._('ID') }}</th>
-            <th data-column-id="name" data-type="string">{{ lang._('Name') }}</th>
-            <th data-column-id="server" data-type="string">{{ lang._('Server') }}</th>
-            <th data-column-id="commands" data-width="7em" data-formatter="commands" data-sortable="false">{{ lang._('Edit') }} | {{ lang._('Delete') }}</th>
-        </tr>
-        </thead>
-        <tbody>
-        </tbody>
-        <tfoot>
-        <tr>
-            <td></td>
-            <td>
-                <button data-action="add" type="button" class="btn btn-xs btn-primary"><span class="fa fa-fw fa-plus"></span></button>
-                <button data-action="deleteSelected" type="button" class="btn btn-xs btn-default"><span class="fa fa-fw fa-trash-o"></span></button>
-            </td>
-        </tr>
-        </tfoot>
-    </table>
+    {{ partial('layout_partials/base_bootgrid_table', formGridDest)}}
 </div>
 
 <div class="content-box __mb">
-    <table class="table table-striped page-header" style="margin-top: 0">
-        <tbody><tr><th>Relays</th><tr></tbody>
-    </table>
-    <table id="grid-relay" class="table table-condensed table-hover table-striped" data-editDialog="DialogRelay">
-        <thead>
-        <tr>
-            <th data-column-id="uuid" data-type="string" data-identifier="true" data-visible="false">{{ lang._('ID') }}</th>
-            <th data-column-id="enabled" data-width="6em" data-type="string" data-formatter="rowtoggle">{{ lang._('Enabled') }}</th>
-            <th data-column-id="status" data-width="6em" data-type="string" data-formatter="statusled">{{ lang._('Status') }}</th>
-            <th data-column-id="interface" data-type="string">{{ lang._('Interface') }}</th>
-            <th data-column-id="destination" data-type="string">{{ lang._('Destination') }}</th>
-            <th data-column-id="agent_info" data-type="string" data-visible="false">{{ lang._('Agent Info') }}</th>
-            <th data-column-id="commands" data-width="7em" data-formatter="commands" data-sortable="false">{{ lang._('Edit') }} | {{ lang._('Delete') }}</th>
-        </tr>
-        </thead>
-        <tbody>
-        </tbody>
-        <tfoot>
-        <tr>
-            <td></td>
-            <td>
-                <button data-action="add" type="button" class="btn btn-xs btn-primary"><span class="fa fa-fw fa-plus"></span></button>
-                <button data-action="deleteSelected" type="button" class="btn btn-xs btn-default"><span class="fa fa-fw fa-trash-o"></span></button>
-            </td>
-        </tr>
-        </tfoot>
-    </table>
+    {{ partial('layout_partials/base_bootgrid_table', formGridRelay)}}
 </div>
 
-<div class="content-box">
-    <div class="col-md-12 __mt __mb">
-        <button class="btn btn-primary" id="reconfigureAct"
-                data-endpoint='/api/dhcrelay/service/reconfigure'
-                data-label="{{ lang._('Apply') }}"
-                data-grid-reload="grid-relay"
-                data-error-title="{{ lang._('Error reconfiguring dhcrelay') }}"
-                type="button">
-        </button>
+<section class="page-content-main">
+    <div class="content-box">
+        <div class="col-md-12">
+            <br/>
+            <button class="btn btn-primary" id="reconfigureAct"
+                    data-endpoint='/api/dhcrelay/service/reconfigure'
+                    data-label="{{ lang._('Apply') }}"
+                    data-grid-reload="grid-relay"
+                    data-error-title="{{ lang._('Error reconfiguring dhcrelay') }}"
+                    type="button">
+            </button>
+            <br/><br/>
+        </div>
     </div>
-</div>
+    <div id="relayChangeMessage" class="alert alert-info" style="display: none" role="alert">
+        {{ lang._('After changing settings, please remember to apply them.') }}
+    </div>
+</section>
 
-{{ partial("layout_partials/base_dialog",['fields':formDialogRelay,'id':'DialogRelay','label':lang._('Edit DHCP relay')])}}
-{{ partial("layout_partials/base_dialog",['fields':formDialogDest,'id':'DialogDest','label':lang._('Edit DHCP destination')])}}
+{{ partial("layout_partials/base_dialog",['fields':formDialogRelay,'id':formGridRelay['edit_dialog_id'],'label':lang._('Edit DHCP relay')])}}
+{{ partial("layout_partials/base_dialog",['fields':formDialogDest,'id':formGridDest['edit_dialog_id'],'label':lang._('Edit DHCP destination')])}}

src/opnsense/mvc/app/controllers/OPNsense/DHCRelay/RelayController.php
@@ -35,7 +35,11 @@ class RelayController extends IndexController
     public function indexAction()
     {
         $this->view->formDialogRelay = $this->getForm('dialogRelay');
+        $this->view->formGridRelay = $this->getFormGrid("dialogRelay", null, "relayChangeMessage");
+
         $this->view->formDialogDest = $this->getForm('dialogDest');
+        $this->view->formGridDest = $this->getFormGrid("dialogDest", null, "relayChangeMessage");
+
         $this->view->pick('OPNsense/DHCRelay/relay');
     }
 }

src/opnsense/mvc/app/controllers/OPNsense/DHCRelay/forms/dialogRelay.xml
@@ -4,6 +4,11 @@
         <label>Enabled</label>
         <type>checkbox</type>
         <help>Enable or disable this relay.</help>
+        <grid_view>
+            <width>6em</width>
+            <type>boolean</type>
+            <formatter>rowtoggle</formatter>
+        </grid_view>
     </field>
     <field>
         <id>relay.interface</id>
@@ -22,5 +27,9 @@
         <label>Agent Information</label>
         <type>checkbox</type>
         <help>Add the relay agent information option.</help>
+        <grid_view>
+            <type>boolean</type>
+            <formatter>boolean</formatter>
+        </grid_view>
     </field>
 </form>

commit 65c44f9bf4f5fa5c16e933f5b71e525c86775859

dhcrelay: Add missing status column, regression from https://github.com/opnsense/core/commit/e1f3d43edcfe65cb3a9cfc56420e204723240be7

src/opnsense/mvc/app/views/OPNsense/DHCRelay/relay.volt
@@ -26,6 +26,10 @@
 
 <script>
 $( document ).ready(function() {
+    // Add the status column since it is not part of dialogRelay.xml
+    const $statusColumn = $('<th data-column-id="status" data-width="6em" data-type="string" data-formatter="statusled">{{ lang._('Status') }}</th>');
+    $('#{{formGridRelay['table_id']}} thead tr th[data-column-id="enabled"]').after($statusColumn);
+
     $("#{{formGridDest['table_id']}}").UIBootgrid({
         search:'/api/dhcrelay/settings/searchDest',
         get:'/api/dhcrelay/settings/getDest/',