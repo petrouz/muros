opnsense/core

commit 128094e10d1c471e7c0d6232bbc4a4fb37fb1870

mvc: Turn camelCase into snake_case in javascript, ACL and volt files (#8920)

src/opnsense/www/js/opnsense_health.js
@@ -91,16 +91,16 @@ class HealthGraph {
     }
 
     exportData() {
-        window.open(`/api/diagnostics/systemhealth/exportAsCSV/${this.currentSystem}/${this.currentDetailLevel}`);
+        window.open(`/api/diagnostics/systemhealth/export_as_csv/${this.currentSystem}/${this.currentDetailLevel}`);
     }
 
     async _fetchRRDList() {
-        const list = await fetch(`/api/diagnostics/systemhealth/getRRDlist`).then(response => response.json());
+        const list = await fetch(`/api/diagnostics/systemhealth/get_rr_dlist`).then(response => response.json());
         return list;
     }
 
     async _fetchData() {
-        const data = await fetch(`/api/diagnostics/systemhealth/getSystemHealth/${this.currentSystem}/${this.currentDetailLevel}`)
+        const data = await fetch(`/api/diagnostics/systemhealth/get_system_health/${this.currentSystem}/${this.currentDetailLevel}`)
             .then(response => response.json());
         return data;
     }

src/opnsense/mvc/app/models/OPNsense/Core/ACL/ACL.xml
@@ -16,11 +16,11 @@
             <pattern>api/core/dashboard/*</pattern>
             <pattern>api/diagnostics/cpu_usage/*</pattern>
             <pattern>api/diagnostics/firewall/pf_states</pattern>
-            <pattern>api/diagnostics/system/systemDisk</pattern>
-            <pattern>api/diagnostics/system/systemInformation</pattern>
-            <pattern>api/diagnostics/system/systemResources</pattern>
-            <pattern>api/diagnostics/system/systemTemperature</pattern>
-            <pattern>api/diagnostics/system/systemTime</pattern>
+            <pattern>api/diagnostics/system/system_disk</pattern>
+            <pattern>api/diagnostics/system/system_information</pattern>
+            <pattern>api/diagnostics/system/system_resources</pattern>
+            <pattern>api/diagnostics/system/system_temperature</pattern>
+            <pattern>api/diagnostics/system/system_time</pattern>
             <pattern>api/diagnostics/system/system_mbuf</pattern>
             <pattern>api/diagnostics/system/system_swap</pattern>
         </patterns>
@@ -41,7 +41,7 @@
         <name>System: Status</name>
         <patterns>
             <pattern>api/core/system/status*</pattern>
-            <pattern>api/core/system/dismissStatus</pattern>
+            <pattern>api/core/system/dismiss_status</pattern>
         </patterns>
     </page-system-status>
     <page-diagnostics-authentication>
@@ -205,7 +205,7 @@
         <name>Firewall: NAT: Outbound</name>
         <patterns>
             <pattern>firewall_nat_out.php*</pattern>
-            <pattern>api/firewall/category/searchItem*</pattern>
+            <pattern>api/firewall/category/search_item*</pattern>
         </patterns>
     </page-firewall-nat-outbound>
     <page-firewall-nat-outbound-edit>
@@ -218,7 +218,7 @@
         <name>Firewall: NAT: Port Forward</name>
         <patterns>
             <pattern>firewall_nat.php*</pattern>
-            <pattern>api/firewall/category/searchItem*</pattern>
+            <pattern>api/firewall/category/search_item*</pattern>
         </patterns>
     </page-firewall-nat-portforward>
     <page-firewall-nat-portforward-edit>
@@ -231,7 +231,7 @@
         <name>Firewall: Rules</name>
         <patterns>
             <pattern>firewall_rules.php*</pattern>
-            <pattern>api/firewall/category/searchItem*</pattern>
+            <pattern>api/firewall/category/search_item*</pattern>
             <pattern>api/firewall/filter_util/rule_stats*</pattern>
         </patterns>
     </page-firewall-rules>
@@ -278,9 +278,9 @@
         <name>Diagnostics: Logs: Firewall: Live View</name>
         <patterns>
             <pattern>ui/diagnostics/firewall/log*</pattern>
-            <pattern>api/diagnostics/firewall/streamLog</pattern>
+            <pattern>api/diagnostics/firewall/stream_log</pattern>
             <pattern>api/diagnostics/firewall/log*</pattern>
-            <pattern>api/diagnostics/interface/getInterfaceNames</pattern>
+            <pattern>api/diagnostics/interface/get_interface_names</pattern>
             <pattern>api/diagnostics/dns/reverse_lookup*</pattern>
             <pattern>api/diagnostics/lvtemplate/*</pattern>
         </patterns>

src/opnsense/mvc/app/models/OPNsense/Diagnostics/ACL/ACL.xml
@@ -3,7 +3,7 @@
         <name>Diagnostics: ARP Table</name>
         <patterns>
             <pattern>ui/diagnostics/interface/arp/*</pattern>
-            <pattern>api/diagnostics/interface/getArp*</pattern>
+            <pattern>api/diagnostics/interface/get_arp*</pattern>
             <pattern>api/diagnostics/interface/search_arp*</pattern>
         </patterns>
     </page-diagnostics-arptable>
@@ -11,7 +11,7 @@
         <name>Diagnostics: NDP Table</name>
         <patterns>
             <pattern>ui/diagnostics/interface/ndp/*</pattern>
-            <pattern>api/diagnostics/interface/getNdp*</pattern>
+            <pattern>api/diagnostics/interface/get_ndp*</pattern>
             <pattern>api/diagnostics/interface/search_ndp*</pattern>
         </patterns>
     </page-diagnostics-ndptable>
@@ -19,16 +19,16 @@
         <name>Diagnostics: Netstat</name>
         <patterns>
             <pattern>ui/diagnostics/interface/netstat/*</pattern>
-            <pattern>api/diagnostics/interface/getInterfaceStatistics*</pattern>
-            <pattern>api/diagnostics/interface/getProtocolStatistics*</pattern>
-            <pattern>api/diagnostics/interface/getSocketStatistics*</pattern>
+            <pattern>api/diagnostics/interface/get_interface_statistics*</pattern>
+            <pattern>api/diagnostics/interface/get_protocol_statistics*</pattern>
+            <pattern>api/diagnostics/interface/get_socket_statistics*</pattern>
         </patterns>
     </page-diagnostics-netstat>
     <page-diagnostics-routingtables>
         <name>Diagnostics: Routing tables</name>
         <patterns>
             <pattern>ui/diagnostics/interface/routes/*</pattern>
-            <pattern>api/diagnostics/interface/getRoutes*</pattern>
+            <pattern>api/diagnostics/interface/get_routes*</pattern>
         </patterns>
     </page-diagnostics-routingtables>
     <page-diagnostics-system-activity>

src/opnsense/mvc/app/models/OPNsense/Unbound/ACL/ACL.xml
@@ -24,24 +24,24 @@
         <name>Services: Unbound DNS: Edit Host and Domain Override</name>
         <patterns>
             <pattern>ui/unbound/overrides</pattern>
-            <pattern>api/unbound/settings/searchHostOverride/*</pattern>
-            <pattern>api/unbound/settings/getHostOverride/*</pattern>
-            <pattern>api/unbound/settings/setHostOverride/*</pattern>
-            <pattern>api/unbound/settings/addHostOverride/*</pattern>
-            <pattern>api/unbound/settings/delHostOverride/*</pattern>
-            <pattern>api/unbound/settings/toggleHostOverride/*</pattern>
-            <pattern>api/unbound/settings/searchHostAlias/*</pattern>
-            <pattern>api/unbound/settings/getHostAlias/*</pattern>
-            <pattern>api/unbound/settings/setHostAlias/*</pattern>
-            <pattern>api/unbound/settings/addHostAlias/*</pattern>
-            <pattern>api/unbound/settings/delHostAlias/*</pattern>
-            <pattern>api/unbound/settings/toggleHostAlias/*</pattern>
-            <pattern>api/unbound/settings/searchDomainOverride/*</pattern>
-            <pattern>api/unbound/settings/getDomainOverride/*</pattern>
-            <pattern>api/unbound/settings/setDomainOverride/*</pattern>
-            <pattern>api/unbound/settings/addDomainOverride/*</pattern>
-            <pattern>api/unbound/settings/delDomainOverride/*</pattern>
-            <pattern>api/unbound/settings/toggleDomainOverride/*</pattern>
+            <pattern>api/unbound/settings/search_host_override/*</pattern>
+            <pattern>api/unbound/settings/get_host_override/*</pattern>
+            <pattern>api/unbound/settings/set_host_override/*</pattern>
+            <pattern>api/unbound/settings/add_host_override/*</pattern>
+            <pattern>api/unbound/settings/del_host_override/*</pattern>
+            <pattern>api/unbound/settings/toggle_host_override/*</pattern>
+            <pattern>api/unbound/settings/search_host_alias/*</pattern>
+            <pattern>api/unbound/settings/get_host_alias/*</pattern>
+            <pattern>api/unbound/settings/set_host_alias/*</pattern>
+            <pattern>api/unbound/settings/add_host_alias/*</pattern>
+            <pattern>api/unbound/settings/del_host_alias/*</pattern>
+            <pattern>api/unbound/settings/toggle_host_alias/*</pattern>
+            <pattern>api/unbound/settings/search_domain_override/*</pattern>
+            <pattern>api/unbound/settings/get_domain_override/*</pattern>
+            <pattern>api/unbound/settings/set_domain_override/*</pattern>
+            <pattern>api/unbound/settings/add_domain_override/*</pattern>
+            <pattern>api/unbound/settings/del_domain_override/*</pattern>
+            <pattern>api/unbound/settings/toggle_domain_override/*</pattern>
         </patterns>
     </page-services-dnsresolver-overrides>
     <page-diagnostics-logs-resolver>

src/opnsense/mvc/app/views/OPNsense/Diagnostics/networkinsight.volt
@@ -127,7 +127,7 @@
       function chart_interface_totals() {
           var selected_time = get_time_select();
           const fetch_params = selected_time.from + '/' + selected_time.to + '/' + selected_time.resolution + '/if,direction' ;
-          ajaxGet('/api/diagnostics/networkinsight/timeserie/FlowInterfaceTotals/bps/' + fetch_params,{},function(data,status){
+          ajaxGet('/api/diagnostics/networkinsight/timeserie/flow_interface_totals/bps/' + fetch_params,{},function(data,status){
               $.each(['chart_intf_in', 'chart_intf_out'], function(idx, target) {
                   let direction = target == 'chart_intf_in' ? 'in' : 'out';
                   let datasets = [];
@@ -224,7 +224,7 @@
         }
         var selected_time = get_time_select();
         var time_url = selected_time.from + '/' + selected_time.to;
-        ajaxGet('/api/diagnostics/networkinsight/top/FlowDstPortTotals/'+time_url+'/dst_port,protocol/octets/25/',
+        ajaxGet('/api/diagnostics/networkinsight/top/flow_dst_port_totals/'+time_url+'/dst_port,protocol/octets/25/',
             {'filter_field': 'if', 'filter_value': $('#interface_select').val()}, function(data, status){
             if (status == 'success'){
               nv.addGraph(function() {
@@ -283,7 +283,7 @@
         }
         var selected_time = get_time_select();
         var time_url = selected_time.from + '/' + selected_time.to;
-        ajaxGet('/api/diagnostics/networkinsight/top/FlowSourceAddrTotals/'+time_url+'/src_addr/octets/25/',
+        ajaxGet('/api/diagnostics/networkinsight/top/flow_source_addr_totals/'+time_url+'/src_addr/octets/25/',
             {'filter_field': 'if', 'filter_value': $('#interface_select').val()}, function(data, status){
             if (status == 'success'){
               let add_src_pie = function(chart_data_in) {
@@ -375,7 +375,7 @@
         var selected_time = get_time_select();
         var time_url = selected_time.from + '/' + selected_time.to;
         measures.map(function(measure){
-          ajaxGet('/api/diagnostics/networkinsight/top/FlowInterfaceTotals/'+time_url+'/direction/'+measure+'/25/',
+          ajaxGet('/api/diagnostics/networkinsight/top/flow_interface_totals/'+time_url+'/direction/'+measure+'/25/',
               {'filter_field': 'if', 'filter_value': $('#interface_select').val()}, function(data, status){
                 var total_in = 0;
                 var total_out = 0;
@@ -431,7 +431,7 @@
         }
 
         var time_url = $("#date_detail_from").val() + '/' +  $("#date_detail_to").val();
-        ajaxGet('/api/diagnostics/networkinsight/top/FlowSourceAddrDetails/'+time_url+'/service_port,protocol,if,src_addr,dst_addr/octets/100/',
+        ajaxGet('/api/diagnostics/networkinsight/top/flow_source_addr_details/'+time_url+'/service_port,protocol,if,src_addr,dst_addr/octets/100/',
             {'filter_field': filters['filter_field'].join(','), 'filter_value': filters['filter_value'].join(',')}, function(data, status){
             if (status == 'success'){
                 let html = [];

src/opnsense/www/js/opnsense_status.js
@@ -188,7 +188,7 @@ class StatusDialog {
 
                 $message.find('#dismiss-' + shortname).on('click', function (e) {
                     e.preventDefault();
-                    $.ajax('/api/core/system/dismissStatus', {
+                    $.ajax('/api/core/system/dismiss_status', {
                         type: 'post',
                         data: {'subject': shortname},
                         dialogRef: dialog,

src/opnsense/www/js/opnsense_widget_manager.js
@@ -121,7 +121,7 @@ class WidgetManager  {
     }
 
     async _loadWidgets() {
-        const response = await $.ajax('/api/core/dashboard/getDashboard', {
+        const response = await $.ajax('/api/core/dashboard/get_dashboard', {
             type: 'GET',
             dataType: 'json',
             contentType: 'application/json'
@@ -604,7 +604,7 @@ class WidgetManager  {
     }
 
     __restoreDefaults(dialog) {
-        $.ajax({type: "POST", url: "/api/core/dashboard/restoreDefaults"}).done((response) => {
+        $.ajax({type: "POST", url: "/api/core/dashboard/restore_defaults"}).done((response) => {
             if (response['result'] == 'failed') {
                 console.error('Failed to restore default widgets');
                 if (dialog !== undefined) {
@@ -676,7 +676,7 @@ class WidgetManager  {
 
         $.ajax({
             type: "POST",
-            url: "/api/core/dashboard/saveWidgets",
+            url: "/api/core/dashboard/save_widgets",
             dataType: "json",
             contentType: "application/json; charset=utf-8",
             data: JSON.stringify(payload),

src/opnsense/www/js/widgets/Disk.js
@@ -153,7 +153,7 @@ export default class Disk extends BaseGaugeWidget {
     }
 
     async onWidgetTick() {
-        const data = await this.ajaxCall('/api/diagnostics/system/systemDisk');
+        const data = await this.ajaxCall('/api/diagnostics/system/system_disk');
         if (data.devices !== undefined) {
             let set = this.detailed_chart.config.data;
             let init = set.labels.length === 0;

petrouz/muros

commit 053ba003501713ad28d331b632875acda22df6f4

mvc: Turn camelCase into snake_case, minor regression in https://github.com/opnsense/core/pull/8920, rename method for readability

src/opnsense/mvc/app/controllers/OPNsense/Diagnostics/Api/SystemhealthController.php
@@ -73,7 +73,7 @@ class SystemhealthController extends ApiControllerBase
      * retrieve Available RRD data
      * @return array
      */
-    public function getRRDlistAction()
+    public function getRrdListAction()
     {
         # Source of data: filelisting of /var/db/rrd/*.rrd
         $result = ['data' => []];

src/opnsense/www/js/opnsense_health.js
@@ -95,7 +95,7 @@ class HealthGraph {
     }
 
     async _fetchRRDList() {
-        const list = await fetch(`/api/diagnostics/systemhealth/get_rr_dlist`).then(response => response.json());
+        const list = await fetch(`/api/diagnostics/systemhealth/get_rrd_list`).then(response => response.json());
         return list;
     }
 