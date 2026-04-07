bb3ced485059555163fd110a594b45f431ba8156
Petrouz <jpzi@proton.me>
2026-04-07 20:33:22 +0200

    src: whitespace sweep

--- Scripts/class-import.sh
+++ Scripts/class-import.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright (c) 2026 Deciso B.V. 
+# Copyright (c) 2026 Deciso B.V.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
