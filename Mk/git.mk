27ab54eadfa24de185de00c124768bb83b09d3f9
Petrouz <jpzi@proton.me>
2026-04-15 09:52:35 +0200

    make: a full file name match should be opened right away
    Noticed while adding this to changelog.git as it prints 26.1, 26.1.1, etc
    when trying to open "26.1" which is more likely what we want.  If we don't
    have an exact match the list is still displayed as before.

--- Mk/git.mk
+++ Mk/git.mk
@@ -1,4 +1,4 @@
-# Copyright (c) 2025 Franco Fichtner <franco@opnsense.org>
+# Copyright (c) 2025-2026 Franco Fichtner <franco@opnsense.org>
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -155,6 +155,11 @@ vim:
 		FOUND="$$(echo "$${FOUND}" | grep -iF "$$(dirname '${vim_ARG}')")"; \
 	fi; \
 	if [ -n "$${FOUND}" ]; then \
+		MATCH="$$(echo "$${FOUND}" | grep -i "/$$(basename '${vim_ARG}')$$")"; \
+		if [ -n "$${MATCH}" ]; then \
+			echo $${MATCH}; \
+			FOUND="$${MATCH}"; \
+		fi; \
 		if [ "$$(echo "$${FOUND}" | wc -l | awk '{ print $$1 }')" = "1" ]; then \
 			${VIM} "$${FOUND}"; \
 			${PHPBIN} -l "$${FOUND}"; \
