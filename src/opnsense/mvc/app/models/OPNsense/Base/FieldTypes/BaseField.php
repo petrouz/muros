ca294d7486a1e36de10e6ceff524b5b0c0617e6c
Petrouz <jpzi@proton.me>
2026-04-27 08:20:52 +0200

    mvc: BaseField: extend count() for value-based field types
    So getValues() already uses isSet() which makes the count of
    set values in the field type correct.  Add the tests to prove
    it.

--- src/opnsense/mvc/app/models/OPNsense/Base/FieldTypes/BaseField.php
+++ src/opnsense/mvc/app/models/OPNsense/Base/FieldTypes/BaseField.php
@@ -340,13 +340,13 @@ abstract class BaseField
     }
 
     /**
-     * count the number of (non virtual) child nodes or 1 for the current node
+     * count the number of (non virtual) child nodes or the number of
+     * set elements on field types with values
      * @return int count
      */
     public function count(): int
     {
-        /* XXX could use count($this->getValues()) if we fix BaseField's version to reject empty */
-        return $this->isContainer() ? iterator_count($this->iterateItems()) : 1;
+        return $this->isContainer() ? iterator_count($this->iterateItems()) : count($this->getValues());
     }
 
     /**
