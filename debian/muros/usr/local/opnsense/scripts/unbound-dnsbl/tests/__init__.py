46e20a002e11e8c489af34b9880348bfb82b78e5
Petrouz <jpzi@proton.me>
2026-06-19 20:58:59 +0200

    themes: replace OPNsense favicon with MurOS mark (amber bars) in both themes and captive portal

--- debian/muros/usr/local/opnsense/scripts/unbound-dnsbl/tests/__init__.py
+++
@@ -1,3 +0,0 @@
-from .dnsbl_tests import *
-from .utils_tests import *
-from .logger_tests import *
--- debian/muros/usr/local/opnsense/service/templates/OPNsense/Dnsmasq/rc.conf.d
+++
@@ -1,6 +0,0 @@
-{% if not helpers.empty('dnsmasq.enable') %}
-dnsmasq_enable="YES"
-dnsmasq_skip="YES"
-{% else %}
-dnsmasq_enable="NO"
-{% endif %}
